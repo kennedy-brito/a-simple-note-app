04f478e4361a2ef37e8f1e9a227b8b78663c0780
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-03-17 19:34:54 -0300

    feat: add initial screen

---
+++ src/app/components/initial/initial.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-initial',
+  standalone: true,
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './initial.component.html',
+  styleUrl: './initial.component.css'
+})
+export class InitialComponent {
+
+}
