8d5811e5d26411a143d9ca4f7b9c626092437c0f
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-03-17 20:40:16 -0300

    feat: add new note component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InitialComponent } from './components/initial/initial.component';
+import { AddNoteComponent } from './components/add-note/add-note.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    InitialComponent
+    InitialComponent,
+    AddNoteComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
