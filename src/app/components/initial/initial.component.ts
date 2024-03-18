e9a853e5f850ed3f79d52bfa252067151946c503
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-03-17 22:09:02 -0300

    feat: add listing notes on initial page

--- src/app/components/initial/initial.component.ts
+++ src/app/components/initial/initial.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { Note } from '../../models/model';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-initial',
@@ -12,6 +14,25 @@ import { RouterLink } from '@angular/router';
   templateUrl: './initial.component.html',
   styleUrl: './initial.component.css'
 })
-export class InitialComponent {
+export class InitialComponent implements OnInit{
 
+  notes: Note[] = []
+
+  constructor(private _noteService: NoteService) { }
+  
+  ngOnInit(): void {
+    this.listNotes()
+  }
+  
+  listNotes()
+  {
+    this._noteService.getNotes().subscribe(
+      {
+        next: next => this.notes = next,
+        error: error => console.log("error")
+      }
+    )
+  }
+
+  
 }
